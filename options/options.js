c1b1353aa00be75e14bb28f0a42ce6ea5e0f2a31
Crowdbotics <team@crowdbotics.com>
2024-03-30 13:46:08 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "purple-tree-47327",
+  url: "https://purple-tree-47327.botics.co",
+  api: "https://purple-tree-47327.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
